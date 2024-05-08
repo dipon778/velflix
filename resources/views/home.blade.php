8ce1f9f32edfb66ec80f591f360f19b2f5672daf
dipon778 <41240922+dipon778@users.noreply.github.com>
2024-05-09 02:39:31 +0600

    Update home.blade.php
    Unlimited to all in the frontpage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -63,7 +63,7 @@
             <div>
                 <div class="text-4xl">Watch everywhere.</div>
                 <div class="text-2xl">
-                    Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV. </div>
+                    Stream All movies and TV shows on your phone, tablet, laptop, and TV. </div>
             </div>
         </div>
         <img width="600" src="{{ asset('img/img3.png') }}" />
